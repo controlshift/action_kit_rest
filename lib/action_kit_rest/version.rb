99351607d25a44f23d5c0c052502e0ef2e316d45
anero <anero79@gmail.com>
2017-08-15 14:43:13 -0300

    New release

--- lib/action_kit_rest/version.rb
+++ lib/action_kit_rest/version.rb
@@ -1,8 +1,8 @@
 module ActionKitRest
   module VERSION
     MAJOR = 0
-    MINOR = 2
-    PATCH = 3
+    MINOR = 3
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
