f56bce0ff7b9128889d9a5f8f42215769630fb5e
Grey Moore <grey@controlshiftlabs.com>
2020-04-24 12:41:16 -0400

    add some basic specs on error stringification, haven't reproduced the error yet

---
+++ spec/lib/action_kit_rest/response/validation_error_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe ActionKitRest::Response::ValidationError do
+  describe '#to_s' do
+    subject { ActionKitRest::Response::ValidationError.new(url: url, body: body) }
+
+    context 'with a normal URL and body' do
+      let(:url) { 'https://actionkit.example.com/rest/v1/page/1' }
+      let(:errors) { {'zip' => 'invalid zip code'} }
+      let(:body) { {errors: errors}.to_json }
+
+      it 'should put information in the string' do
+        expect(subject.to_s).to eq "ActionKitRest::Response::ValidationError \n url: #{url} \n body: #{body} \n errors: #{errors.inspect}"
+      end
+    end
+
+    context 'with special characters in the body' do
+      let(:url) { 'https://actionkit.example.com/rest/v1/page/1' }
+      let(:errors) { {'zip' => '☃'} }
+      let(:body) { {errors: errors}.to_json }
+
+      it 'should put information in the string' do
+        expect(subject.to_s).to eq "ActionKitRest::Response::ValidationError \n url: #{url} \n body: #{body} \n errors: #{errors.inspect}"
+      end
+    end
+  end
+end
